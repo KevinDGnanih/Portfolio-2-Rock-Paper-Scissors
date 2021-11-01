4b2e23191399e2590ac01f697132206ca3cf7cfd
KevinDGnanih <kevin.gnanih@gmail.com>
2021-11-01 09:37:53 +0000

    Back to the original layout of the game and started to add functions to JavaScript file

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Declare constants for DOM elements
+// Possible choices
+// Get the button elements and event listeners to them
+
+const buttons = document.getElementsByClassName("user-choice-area");
+const userScore = document.getElementById("user-score");
+const computerScore = document.getElementById("computer-score");
+const userChoice = document.getElementById("game-area");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper" , "scissors"];
+
+/**
+ * Add event listener to all the buttons
+ */
+for (let button of buttons) {
+    button.addEventListener('click', function(){
+        let userChoice = this.getAttribute("data-choice");
+        runGame(userChoice);
+    });
+}
+
+/**
+ * The main game function. Accepts one parameter, Which 
+ * is the data-choice value of the selected button
+ */
+function runGame(userChoice) {
+
+    userChoice = choices[userChoice]
+}
