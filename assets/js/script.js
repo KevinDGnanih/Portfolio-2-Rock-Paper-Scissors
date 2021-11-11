da308c48e51d032b273c64e0fef9f283c43dd435
KevinDGnanih <kevin.gnanih@gmail.com>
2021-11-11 23:13:20 +0000

    Setting up the rule and the result message part 01

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,30 @@ const userScore = document.getElementById("user-score");
 const computerScore = document.getElementById("computer-score");
 const userChoice = document.getElementById("user-choice-area");
 const computerChoice = document.getElementById("computer-choice-area");
-const userChoiceDisplay = document.getElementById("");
-const computerChoiceDisplay = document.getElementById("");
-let computerMsg;
-let userMsg;
+const userChoiceDisplay = document.createElement("");
+const computerChoiceDisplay = document.createElement("");
 const messages = document.getElementById("messages");
 const choices = ["Rock", "Paper" , "Scissors"];
+
+
+let result = document.getElementBy("result");
+let rule = {
+    'Rock' : {
+        'Rock' : 'draw',
+        'Scissors' : 'win',
+        'Paper' : 'lose',
+    },
+    'Scissors': {
+        'Rock' : 'lose',
+        'Scissors' : 'draw',
+        'Paper' : 'win',
+    },
+    'Paper' : {
+        'Rock' : 'win',
+        'Scissors' : 'lose',
+        'Paper' : 'draw',
+    },
+}
 
 /**
  * Add event listener to all the buttons
@@ -25,8 +43,7 @@ for (let button of buttons) {
 }
 
 /**
- * The main game function. Accepts one parameter, Which 
- * is the data-choice value of the selected button
+ * The main game function
  */
 function runGame(userChoice) {
 
@@ -34,16 +51,10 @@ function runGame(userChoice) {
     `<h2>You choose <span>${userChoice}</span></h2>`;
 
     const num = Math.floor(Math.random() * 3);
-
     
     document.getElementById("message-computer").innerHTML = 
     `<h2>Computer choice is <span>${choices[num]}</span></h2>`;
-
-    
-    
-   
-    /*console.log(num);
-    computerChoiceDisplay.innerHTML = computerChoice;*/
+ 
     
 }
 
